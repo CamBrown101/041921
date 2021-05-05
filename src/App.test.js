3edf7584decff05f4c9b03823e41917807c72b03
cambrown101 <cameron-brown@live.com>
2021-05-04 17:29:18 -0700

    Fix current logo test

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders Sphere logo', () => {
+test("renders Sphere Pups logo", () => {
   render(<App />);
-  const element = screen.getByText(/Sphere/i);
+  const element = screen.getByText("Sphere Pups");
   expect(element).toBeInTheDocument();
 });
